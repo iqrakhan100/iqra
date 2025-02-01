a2752d89dfe530feeabe460bc96e3782d90dda30
Iqra Khan <iqrakhan100@gmail.com>
2025-02-01 08:14:43 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyWidget());
+}
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          title: Text("Container Class"),
+        ),
+        body: Column(
+          children: [
+            SizedBox(height: 20,),
+           Center(
+             child: Container(
+              height: 100,
+              width: 200,
+              color: Colors.red,
+              child: Text("Hello IT",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+             ),
+           )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
